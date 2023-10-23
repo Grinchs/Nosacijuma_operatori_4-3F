69a6556cb3cb3910365a5c3e5ed9301c3bebf3c5
Grinchs <marcisg2345@gmail.com>
2023-10-24 00:00:27 +0300

    Add project files.

---
+++ Nosacijuma_operatori_4-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        Console.Write("Ievadiet vārdu: ");
+        string ievaditaParole = Console.ReadLine();
+
+       
+        if (ievaditaParole.Length == 5)
+        {
+            
+            string pareizaParole = "Gatis";
+
+            // Parbauda, vai ievaditais vards sakrit ar defineto paroli
+            if (ievaditaParole == pareizaParole)
+            {
+                Console.WriteLine("Parole ievadīta pareizi");
+            }
+            else
+            {
+                Console.WriteLine("Parole ievadīta nepareizi");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Jābūt 5 simboliem.");
+        }
+    }
+}
